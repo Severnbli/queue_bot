7e179a2d442a75dc861a013232cb41abf920a3eb
Usevalad <usevaladbuben@gmail.com>
2024-10-20 21:44:04 +0300

    Fix func get_release_queues_info()

--- db/queues_info_table_usage.py
+++ db/queues_info_table_usage.py
@@ -188,16 +188,11 @@ async def simple_get_status_of_queue_info(queue_info_id: int):
 async def get_release_queues_info():
     await cur.execute('SELECT group_id, subject, lesson_type, subgroup, day_of_week '
                       'FROM queues_info '
-                      'WHERE status = ?', ('release',))
+                      'WHERE status = ?', ('prerelease',))
 
     rows = await cur.fetchall()
 
     if len(rows) == 0:
         return sc.NO_QUEUES_IN_RELEASE, None
 
-    queues_info = []
-
-    for row in rows:
-        queues_info.append(row[0])
-
-    return sc.OPERATION_SUCCESS, queues_info
+    return sc.OPERATION_SUCCESS, rows
