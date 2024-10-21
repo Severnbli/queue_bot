9f198dc34d98f0d3bd3745de7932190d5649a543
Usevalad <usevaladbuben@gmail.com>
2024-10-21 16:28:27 +0300

    Better decorators' work. Adding state reset in checked cases

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,5 +1,7 @@
 from functools import wraps
-from aiogram.types import Message, ReplyKeyboardRemove
+
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
 
 import db.users_table_usage as usersdb
 import db.members_table_usage as membersdb
@@ -9,57 +11,82 @@ from markups import reply_markups
 
 def user_exists_required(func):
     @wraps(func)
-    async def wrapper(message: Message, *args, **kwargs):
+    async def wrapper(message: Message, state: FSMContext, *args, **kwargs):
         if not await usersdb.is_user_exist_(message.from_user.id):
+            await state.clear()
+
             output_message = 'Тебя нет в базе моих знаний. Попробуй зарегистрироваться: /start.'
+
             await message.answer(output_message, reply_markup=await reply_markups.get_register_keyboard())
             return
+
         return await func(message, *args, **kwargs)
     return wrapper
 
 
 def user_group_leader_or_depute_required(func):
     @wraps(func)
-    async def wrapper(message: Message, *args, **kwargs):
+    async def wrapper(message: Message, state: FSMContext, *args, **kwargs):
         status_code, user_position = await membersdb.get_user_position_in_group(message.from_user.id)
+
         if status_code != sc.OPERATION_SUCCESS:
+            await state.clear()
+
             output_message = (f'Возникла ошибка при определении твоей роли в группе: '
                               f'{await get_message_about_error(status_code)}.')
             await message.answer(output_message, reply_markup=await reply_markups.get_main_keyboard())
             return
+
         elif user_position not in ['leader', 'depute']:
+            await state.clear()
+
             output_message = 'У тебя недостаточно прав на это действие.'
+
             await message.answer(output_message, reply_markup=await reply_markups.get_main_keyboard())
             return
+
         return await func(message, *args, **kwargs)
     return wrapper
 
 
 def user_group_leader_required(func):
     @wraps(func)
-    async def wrapper(message: Message, *args, **kwargs):
+    async def wrapper(message: Message, state: FSMContext, *args, **kwargs):
         status_code, user_position = await membersdb.get_user_position_in_group(message.from_user.id)
+
         if status_code != sc.OPERATION_SUCCESS:
+            await state.clear()
+
             output_message = (f'Возникла ошибка при определении твоей роли в группе: '
                               f'{await get_message_about_error(status_code)}.')
+
             await message.answer(output_message, reply_markup=await reply_markups.get_main_keyboard())
             return
+
         elif user_position != 'leader':
+            await state.clear()
+
             output_message = 'У тебя недостаточно прав на это действие.'
+
             await message.answer(output_message, reply_markup=await reply_markups.get_main_keyboard())
             return
+
         return await func(message, *args, **kwargs)
     return wrapper
 
 
 def user_in_group_required(func):
     @wraps(func)
-    async def wrapper(message: Message, *args, **kwargs):
+    async def wrapper(message: Message, state: FSMContext, *args, **kwargs):
         is_user_in_group = await membersdb.is_user_in_group_(message.from_user.id)
+
         if not is_user_in_group:
+            await state.clear()
+
             output_message = 'Для выполнения этого действия ты должен быть в группе.'
             await message.answer(output_message, reply_markup=await reply_markups.get_main_keyboard())
             return
+
         return await func(message, *args, **kwargs)
     return wrapper
 
@@ -78,16 +105,25 @@ def user_not_in_group_required(func):
 
 def user_not_group_leader_required(func):
     @wraps(func)
-    async def wrapper(message: Message, *args, **kwargs):
+    async def wrapper(message: Message, state: FSMContext, *args, **kwargs):
         status_code, user_position = await membersdb.get_user_position_in_group(message.from_user.id)
+
         if status_code != sc.OPERATION_SUCCESS:
+            await state.clear()
+
             output_message = (f'Возникла ошибка при определении твоей роли в группе: '
                               f'{await get_message_about_error(status_code)}.')
+
             await message.answer(output_message, reply_markup=await reply_markups.get_main_keyboard())
             return
+
         elif user_position == 'leader':
+            await state.clear()
+
             output_message = 'Ты не можешь совершить это, так как ты лидер.'
             await message.answer(output_message, reply_markup=await reply_markups.get_main_keyboard())
             return
+
         return await func(message, *args, **kwargs)
+
     return wrapper
